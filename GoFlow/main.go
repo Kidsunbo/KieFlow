df13e425ef5c7bbdb43e38cae79c642621731a75
sunbo.kie <sunbo.kie@bytedance.com>
2021-01-18 15:24:32 +0800

    apply go fmt

--- GoFlow/main.go
+++ GoFlow/main.go
@@ -2,59 +2,56 @@ package main
 
 import "fmt"
 
-func Fun1WithoutData(data *DataSet)*Result  {
-	fmt.Println("Fun1WithoutData, ",data)
+func Fun1WithoutData(data *DataSet) *Result {
+	fmt.Println("Fun1WithoutData, ", data)
 	return nil
 }
 
-func Fun2WithoutData(data *DataSet)*Result  {
-	fmt.Println("Fun2WithoutData, ",data)
+func Fun2WithoutData(data *DataSet) *Result {
+	fmt.Println("Fun2WithoutData, ", data)
 	return new(Result)
 }
 
-
-func Fun1WithData(data *DataSet)*Result  {
-	fmt.Println("Fun1WithData, ",data)
+func Fun1WithData(data *DataSet) *Result {
+	fmt.Println("Fun1WithData, ", data)
 	return new(Result)
 }
 
-
-func Fun2WithData(data *DataSet)*Result  {
-	fmt.Println("Fun2WithData, ",data)
+func Fun2WithData(data *DataSet) *Result {
+	fmt.Println("Fun2WithData, ", data)
 	return nil
 }
 
-func PrepareData(data *DataSet,input InputParam)*Result{
-	fmt.Println("PrepareData, data=",data,"input=",input)
+func PrepareData(data *DataSet, input InputParam) *Result {
+	fmt.Println("PrepareData, data=", data, "input=", input)
 	data.Name = "Tom is Tom"
 	result := new(Result)
 	return result
 }
 
-func PrepareDataWithErr(data *DataSet,input InputParam)*Result{
-	fmt.Println("PrepareData, data=",data,"input=",input)
+func PrepareDataWithErr(data *DataSet, input InputParam) *Result {
+	fmt.Println("PrepareData, data=", data, "input=", input)
 	data.Name = "Tom is Tom"
 	result := new(Result)
 	result.StatusCode = 1
 	return result
 }
 
-func BeginLogger(note string,data *DataSet){
-	fmt.Println("[START]", note,"data = ",data)
+func BeginLogger(note string, data *DataSet) {
+	fmt.Println("[START]", note, "data = ", data)
 }
 
-func EndLogger(note string,data *DataSet,result *Result){
-	fmt.Println("[END]", note,"data = ",data, "result=",result)
+func EndLogger(note string, data *DataSet, result *Result) {
+	fmt.Println("[END]", note, "data = ", data, "result=", result)
 }
 
+func main() {
+	result := NewFlow().Do(Fun1WithoutData, Fun2WithoutData).SetNote("开始检查数据").
+		Prepare(InputParam{}, PrepareData).SetNote("开始准备数据").
+		Do(Fun1WithData, Fun2WithData).SetNote("带着数据检查").
+		SetGlobalBeginLogger(BeginLogger).
+		SetGlobalEndLogger(EndLogger).
+		Wait()
 
-func main(){
-	result := NewFlow().Do(Fun1WithoutData,Fun2WithoutData).SetNote("开始检查数据").
-	Prepare(InputParam{},PrepareData).SetNote("开始准备数据").
-	Do(Fun1WithData,Fun2WithData).SetNote("带着数据检查").
-	SetGlobalBeginLogger(BeginLogger).
-	SetGlobalEndLogger(EndLogger).
-	Wait()
-
-	fmt.Println("Result=",result)
+	fmt.Println("Result=", result)
 }
--- GoFlow/structure.go
+++ GoFlow/structure.go
@@ -7,7 +7,7 @@ import "context"
 // [IMPORTANT] Notice that even though Result can be replace with other type, the Err, StatusCode and StatusMsg must be provided
 
 type DataSet struct {
-	Ctx context.Context
+	Ctx  context.Context
 	Name string
 }
 
