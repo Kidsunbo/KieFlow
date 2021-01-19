838e46b80ab01477a3db25fb2b6a25dad66b1d88
sunbo.kie <sunbo.kie@bytedance.com>
2021-01-19 11:21:32 +0800

    fix a little bug

--- GoFlow/goflow/go_flow.go
+++ GoFlow/goflow/go_flow.go
@@ -951,7 +951,7 @@ func (f *FlowEngine) OnFail(functor IOnFailFunc) *FlowEngine {
 	return f
 }
 
-func (f *FlowEngine) OnSuccess(functor IOnFailFunc) *FlowEngine {
+func (f *FlowEngine) OnSuccess(functor IOnSuccessFunc) *FlowEngine {
 	f.onSuccessFunc = functor
 	return f
 }
@@ -1171,7 +1171,7 @@ func (e *ElseFlowEngine) OnFail(functor IOnFailFunc) *ElseFlowEngine {
 	return e
 }
 
-func (e *ElseFlowEngine) OnSuccess(functor IOnFailFunc) *ElseFlowEngine {
+func (e *ElseFlowEngine) OnSuccess(functor IOnSuccessFunc) *ElseFlowEngine {
 	e.onSuccessFunc = functor
 	return e
 }
